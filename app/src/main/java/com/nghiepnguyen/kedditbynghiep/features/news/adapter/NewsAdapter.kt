06c0ea307148f23214194a9d4f56a2b34e8a252b
Nghiep Nguyen <nghiep.nguyen@inspireventures.com>
2017-06-04 14:35:11 +0700

    creating NewsAdapter

---
+++ app/src/main/java/com/nghiepnguyen/kedditbynghiep/features/news/adapter/NewsAdapter.kt
@@ -0,0 +1,43 @@
+package com.nghiepnguyen.kedditbynghiep.features.news.adapter
+
+import android.support.v4.util.SparseArrayCompat
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.nghiepnguyen.kedditbynghiep.commons.adapter.AdapterConstants
+import com.nghiepnguyen.kedditbynghiep.commons.adapter.ViewType
+import com.nghiepnguyen.kedditbynghiep.commons.adapter.ViewTypeDelegateAdapter
+import java.util.*
+
+/**
+ * Created by W10-PRO on 03-Jun-17.
+ */
+
+internal class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var items: ArrayList<ViewType>
+    private var delegateAdapters = SparseArrayCompat<ViewTypeDelegateAdapter>()
+    private var loadingItem = object : ViewType {
+        override fun getViewType() = AdapterConstants.LOADING
+    }
+
+    init {
+        delegateAdapters.put(AdapterConstants.LOADING, LoadingDelegateAdapter())
+        items = ArrayList()
+        items.add(loadingItem)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return delegateAdapters.get(viewType).onCreateViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        delegateAdapters.get(getItemViewType(position)).onBindViewHolder(holder, this.items[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return this.items[position].getViewType()
+    }
+}
